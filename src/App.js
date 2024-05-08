c730e1bb666c01f4bb5cf13ff8906f859c9bbf95
Mario Oliveira <mjribeiro@triscal.com.br>
2024-05-08 15:52:46 -0300

    STARTUP: API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+/**
+    * @description      : 
+    * @author           : Mário Jorge Ribeiro
+    * @group            : 
+    * @created          : 08/05/2024 - 11:53:34
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 08/05/2024
+    * - Author          : Mário Jorge Ribeiro
+    * - Modification    : 
+**/
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
+const socket = io('http://localhost:8080');
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		socket.on('products', (data) => {
+			console.log('data:', data);
+			setProducts(data);
+		});
+		// return () => socket.disconnect();
+	});
+
+	return (
+		<div>
+			<h1>Lista de Produtos:</h1>
+			<ul>
+				{products.map(product => (
+					<li key={product.id} className='product' onClick={() => alert(`${product.name} - R$${product.price}`)}>
+						<div className='product-wrap'>
+							<div className='title-wrap'>{product.name}</div>
+							<div className='price-wrap'>{`R$${parseFloat(product.price).toFixed(2)}`}</div>
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
